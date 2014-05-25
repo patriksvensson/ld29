39533de1be24c160e184a23bde608ee77ceeb153
Patrik Svensson <patrik@patriksvensson.se>
2014-05-25 23:46:45 +0200

    Updated Lunt.

--- src/Surface.Pipeline.Debugger/Program.cs
+++ src/Surface.Pipeline.Debugger/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Lunt;
 using Lunt.Debugging;
-using Lunt.Diagnostics;
-using Lunt.IO;
-using Lunt.Runtime;
 using Surface.Pipeline.Importers;
-using Path = System.IO.Path;
 
 namespace Surface.Pipeline.Debugger
 {
@@ -22,13 +15,8 @@ namespace Surface.Pipeline.Debugger
             Console.WriteLine("Surface Pipeline Debugger\r\n");
             Console.ResetColor();
 
-            var assembly = typeof(TextureImporter).Assembly;
-            var bootstrapper = new DebuggerBootstrapper(assembly);
-
-            using (var debugger = new Lunt.Debugging.Debugger(bootstrapper))
-            {
-                debugger.Run("../../../../assets/build.xml");
-            }
+            var debugger = new LuntDebugger(typeof(TextureImporter).Assembly);
+            debugger.Run("../../../../assets/build.xml");
         }
     }
 }
